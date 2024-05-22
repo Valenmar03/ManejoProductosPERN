b0a18933cd8da6510bb75133780874fbefe0632d
Valenmar03 <valenmrtnz8@gmail.com>
2024-05-22 15:35:09 -0300

    Conectando Express a base

---
+++ src/config/db.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize('postgres://rest_api_node_ts_fea6_user:zHxXE6L04FvDHTUkPRAaBUt17jmsZc5N@dpg-cp72vdicn0vc73a3i9og-a.oregon-postgres.render.com/rest_api_node_ts_fea6?ssl=true')
+
+export default db;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from "express";
 import router from "./router";
+import db from "./config/db";
+
+async function connectDB(){
+    try{
+        await db.authenticate()
+        db.sync()
+        console.log('Successfully connected to database')
+    }catch(error){
+        console.log(error)
+        console.log('Error connecting to database...')
+    }
+}
+connectDB()
+
 
 const server = express();
 
