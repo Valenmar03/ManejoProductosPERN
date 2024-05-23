a81f7cecdf8e184fea36fb5ebae81f75d4f5480f
Valenmar03 <valenmrtnz8@gmail.com>
2024-05-23 10:05:20 -0300

    get, getById, updateProduct, updateAvailability endpoints

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,7 @@
 import { Request, Response} from 'express'
 import { validationResult } from 'express-validator'
 import Product from '../models/Product.model'
+import { AfterValidate } from 'sequelize-typescript'
 
 export const createProduct = async (req : Request, res : Response) => {
     try {
@@ -9,4 +10,65 @@ export const createProduct = async (req : Request, res : Response) => {
     } catch (error) {
         console.log(error)
     }
+}
+
+export const getProducts = async (req : Request, res : Response) => {
+    try {
+        const products = await Product.findAll()
+        if(!products){
+            return res.status(404).json({
+                error: 'No products found'
+            })
+        }
+
+        res.json({data: products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getProductById = async (req : Request, res : Response) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+        if(!product){
+            return res.status(404).json({
+                error: 'Product not found'
+            })
+        }
+
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateProduct = async (req : Request, res : Response) => {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+    if(!product){
+        return res.status(404).json({
+            error: 'Product not found'
+        })
+    }
+
+    await product.update(req.body)
+    await product.save()
+
+    return res.json({data: product})
+}
+
+export const updateProductAvailability = async (req : Request, res : Response) => {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+    if(!product){
+        return res.status(404).json({
+            error: 'Product not found'
+        })
+    }
+
+    product.availability = !product.dataValues.availability
+    await product.update(product)
+    await product.save()
+    return res.json({data: product})
 }
--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,56 @@
 import { Router } from "express";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { handleInputErrors } from "./middleware";
-import { createProduct } from "./handlers/product";
+import {
+   createProduct,
+   getProductById,
+   getProducts,
+   updateProduct,
+   updateProductAvailability,
+} from "./handlers/product";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-   res.send("Desde GET");
-});
-
 router.post(
    "/",
-
    body("name").notEmpty().withMessage("Product cant be empty"),
-
    body("price")
       .isNumeric()
+      .withMessage("Product should be numeric")
       .notEmpty()
+      .withMessage("Price can't be empty")
       .custom((value) => value > 0)
       .withMessage("Price not valid"),
-    handleInputErrors,
+   handleInputErrors,
    createProduct
 );
+
+router.get("/", getProducts);
+
+router.get(
+   "/:id",
+   param("id").isInt().withMessage("Id not valid"),
+   handleInputErrors,
+   getProductById
+);
+
+router.put(
+   "/:id",
+   body("name").notEmpty().withMessage("Product cant be empty"),
+   body("price")
+      .isNumeric()
+      .withMessage("Product should be numeric")
+      .notEmpty()
+      .withMessage("Price can't be empty")
+      .custom((value) => value > 0)
+      .withMessage("Price not valid"),
+   body("availability").isBoolean().withMessage("Availability value not valid"),
+   handleInputErrors,
+   updateProduct
+);
+
+router.patch("/:id", 
+    updateProductAvailability
+)
 
 export default router;
