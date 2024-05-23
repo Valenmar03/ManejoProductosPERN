b39f1373c3484ae94e9e3ea2151c893947812432
Valenmar03 <valenmrtnz8@gmail.com>
2024-05-23 08:06:08 -0300

    move validation into router

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,23 +1,15 @@
 import { Request, Response} from 'express'
-import { check, validationResult} from 'express-validator'
+import { validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req : Request, res : Response) =>{
 
-    await check('name').notEmpty().withMessage('Product cant be empty').run(req)
-    await check('price')
-        .isNumeric()
-        .notEmpty()
-        .custom(value => value > 0)
-        .withMessage('Price not valid')
-        .run(req)
 
     let errors = validationResult(req)
 
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array()})
     }
-
     const product = await Product.create(req.body)
     res.json({data: product}) 
 }
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,25 @@
-import { Router } from 'express'
-import { createProduct } from './handlers/product'
+import { Router } from "express";
+import { body } from "express-validator";
+import { createProduct } from "./handlers/product";
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req, res) =>{
-    res.send('Desde GET')
-})
+router.get("/", (req, res) => {
+   res.send("Desde GET");
+});
 
-router.post('/', createProduct)
+router.post(
+   "/",
 
-export default router
+   body("name").notEmpty().withMessage("Product cant be empty"),
+
+   body("price")
+      .isNumeric()
+      .notEmpty()
+      .custom((value) => value > 0)
+      .withMessage("Price not valid"),
+
+   createProduct
+);
+
+export default router;
