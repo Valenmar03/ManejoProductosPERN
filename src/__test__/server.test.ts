6be51e3efd3fbb9fa3840ad33bf722e81093a2a5
Valenmar03 <valenmrtnz8@gmail.com>
2024-05-25 17:22:38 -0300

    Create test for createProduct

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -4,7 +4,6 @@ import server from '../server';
 describe('GET /api',() => {
     test('Should send back a json response', async () =>{
         const res = await request(server).get('/api');
-        
         expect(res.status).toBe(200)
     })
 })
--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -1,2 +1,36 @@
 import request from 'supertest'
 import server from '../../server'
+
+
+describe('POST /api/products', () => {
+    test('Should display validation errors', async () => {
+        const res = await request(server).post('/api/products').send({})
+        expect(res.status).toBe(400)
+        expect(res.body).toHaveProperty('errors')
+        expect(res.body.errors).toHaveLength(4)
+        
+        expect(res.status).not.toBe(404)
+        expect(res.body.errors).not.toHaveLength(2)
+    })
+    test('Should validate that the price is higher than 0', async () => {
+        const res = await request(server).post('/api/products').send({
+            name: 'Monitor Test',
+            price: 0,
+        })
+        expect(res.status).toBe(400)
+        expect(res.body).toHaveProperty('errors')
+        expect(res.body.errors).toHaveLength(1)
+        
+        expect(res.status).not.toBe(404)
+        expect(res.body.errors).not.toHaveLength(2)
+    })
+    test('Should create a new product',async  () => {
+        const res = await request(server).post('/api/products').send({
+            name: 'Mouse Test',
+            price: 50,
+        })
+
+        expect(res.status).toBe(201)
+        expect(res.body).toHaveProperty('data')
+    })
+})
