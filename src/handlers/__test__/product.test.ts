3805fa41446a74e22d26df53d6c57ba1b9eabadf
Valenmar03 <valenmrtnz8@gmail.com>
2024-05-25 22:24:40 -0300

    Make more tests and execute coverage

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -1,36 +1,121 @@
-import request from 'supertest'
-import server from '../../server'
+import request from "supertest";
+import server from "../../server";
 
+describe("POST /api/products", () => {
+   test("Should display validation errors", async () => {
+      const res = await request(server).post("/api/products").send({});
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toHaveLength(4);
 
-describe('POST /api/products', () => {
-    test('Should display validation errors', async () => {
-        const res = await request(server).post('/api/products').send({})
-        expect(res.status).toBe(400)
-        expect(res.body).toHaveProperty('errors')
-        expect(res.body.errors).toHaveLength(4)
-        
-        expect(res.status).not.toBe(404)
-        expect(res.body.errors).not.toHaveLength(2)
-    })
-    test('Should validate that the price is higher than 0', async () => {
-        const res = await request(server).post('/api/products').send({
-            name: 'Monitor Test',
-            price: 0,
-        })
-        expect(res.status).toBe(400)
-        expect(res.body).toHaveProperty('errors')
-        expect(res.body.errors).toHaveLength(1)
-        
-        expect(res.status).not.toBe(404)
-        expect(res.body.errors).not.toHaveLength(2)
-    })
-    test('Should create a new product',async  () => {
-        const res = await request(server).post('/api/products').send({
-            name: 'Mouse Test',
-            price: 50,
-        })
+      expect(res.status).not.toBe(404);
+      expect(res.body.errors).not.toHaveLength(2);
+   });
+   test("Should validate that the price is higher than 0", async () => {
+      const res = await request(server).post("/api/products").send({
+         name: "Monitor Test",
+         price: 0,
+      });
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toHaveLength(1);
 
-        expect(res.status).toBe(201)
-        expect(res.body).toHaveProperty('data')
-    })
+      expect(res.status).not.toBe(404);
+      expect(res.body.errors).not.toHaveLength(2);
+   });
+   test("Should create a new product", async () => {
+      const res = await request(server).post("/api/products").send({
+         name: "Mouse Test",
+         price: 50,
+      });
+
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty("data");
+   });
+});
+
+describe("GET /api/products", () => {
+   test("Should check that url /api/products exists", async () => {
+      const res = await request(server).get("/api/products");
+      expect(res.status).not.toBe(404);
+   });
+   test("Should GET a json response with all the products", async () => {
+      const res = await request(server).get("/api/products");
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("data");
+      expect(res.headers["content-type"]).toMatch(/json/);
+
+      expect(res.body).not.toHaveProperty("errors");
+   });
+});
+
+describe("GET /api/products/:id", () => {
+   test("Should return a 404 status if product doesnt exist", async () => {
+      const productId = 2000;
+      const res = await request(server).get(`/api/products/${productId}`);
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+      expect(res.body.error).toBe("Product not found");
+   });
+   test("Should check if productId is valid", async () => {
+      const res = await request(server).get("/api/products/not-valid-url");
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+   });
+   test("Should return a json response with the product", async () => {
+      const res = await request(server).get(`/api/products/1`);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("data");
+   });
+});
+
+describe("PUT /api/products/:id", () => {
+   test("Should display validation error messages when updating a product", async () => {
+      const res = await request(server).put("/api/products/1").send({});
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toBeTruthy();
+      expect(res.body.errors).toHaveLength(5);
+
+      expect(res.status).not.toBe(200);
+   });
+
+   test("Should validate that the price is higher than 0", async () => {
+      const res = await request(server).put("/api/products/1").send({
+         name: "Monitor",
+         price: -300,
+         availability: true,
+      });
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty("errors");
+      expect(res.body.errors).toBeTruthy();
+      expect(res.body.errors).toHaveLength(1);
+
+      expect(res.status).not.toBe(200);
+   });
+});
+
+describe("DELETE /api/products/:id", () => {
+   test("Should check a valid ID", async () => {
+      const res = await request(server).delete("/api/products/id-not-valid")
+
+      expect(res.status).toBe(400)
+      expect(res.body).toHaveProperty('errors')
+      expect(res.body.errors[0].msg).toBe('Id not valid')
+   })
+   test("Should return a 404 status if product doesnt exist", async () => {
+      const productId = 2000;
+      const res = await request(server).get(`/api/products/${productId}`);
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty("error");
+      expect(res.body.error).toBe("Product not found");
+   });
+   test("Should delete a product", async () => {
+      const res = await request(server).delete("/api/products/1")
+      expect(res.status).toBe(200)
+      expect(res.body.message).toBe("Product successfully deleted")
+   })
 })
--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,43 +2,31 @@ import { Request, Response} from 'express'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req : Request, res : Response) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(201).json({data: product}) 
-    } catch (error) {
-        console.log(error)
-    }
+    const product = await Product.create(req.body)
+    res.status(201).json({data: product}) 
 }
 
 export const getProducts = async (req : Request, res : Response) => {
-    try {
-        const products = await Product.findAll()
-        if(!products){
-            return res.status(404).json({
-                error: 'No products found'
-            })
-        }
-
-        res.json({data: products})
-    } catch (error) {
-        console.log(error)
+    const products = await Product.findAll()
+    if(!products){
+        return res.status(404).json({
+            error: 'No products found'
+        })
     }
+
+    res.json({data: products})
 }
 
 export const getProductById = async (req : Request, res : Response) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findByPk(id)
-        if(!product){
-            return res.status(404).json({
-                error: 'Product not found'
-            })
-        }
-
-        res.json({data: product})
-    } catch (error) {
-        console.log(error)
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+    if(!product){
+        return res.status(404).json({
+            error: 'Product not found'
+        })
     }
+
+    res.json({data: product})
 }
 
 export const updateProduct = async (req : Request, res : Response) => {
